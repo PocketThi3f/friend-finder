afc4de69945ac7df688c7d260aeba5aa34616792
PocketThi3f <krslai@yahoo.com>
2017-01-11 14:34:48 -0800

    surveyupdates

---
+++ app/data/finder.js
@@ -0,0 +1,24 @@
+// Friends You Can Keep (DATA)
+// ============================================================= 
+var friendsArr = [
+	{
+	  "name":"The Animaniacs",
+	  "photo":"http://akns-images.eonline.com/eol_images/Entire_Site/201637/rs_560x415-160407101644-1024-animaniacs-coming-to-netflix-040716.jpg",
+	  "scores":[]
+	},
+
+	{
+	  "name":"Pinky And The Brain",
+	  "photo":"https://s-media-cache-ak0.pinimg.com/originals/b0/0d/8c/b00d8cc026c21ead1ccaaf204e87bcce.jpg",
+	  "scores":[]
+	},
+
+	{
+	  "name":"Ahmed",
+	  "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+	  "scores":[]
+	}
+
+];
+
+module.exports = friendsArr;
