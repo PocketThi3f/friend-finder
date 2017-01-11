f7f703c1a980ec55df3df2e22eaa99cba8e951c1
PocketThi3f <krslai@yahoo.com>
2017-01-11 15:05:24 -0800

    updating server script

--- server.js
+++ server.js
@@ -18,50 +18,8 @@ app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 // Routes
 // =============================================================
 // Basic route that sends the user first to the AJAX Page
-app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "view.html"));
-});
-
-app.get("/add", function(req, res) {
-  res.sendFile(path.join(__dirname, "add.html"));
-});
-
-app.get("/all", function(req, res) {
-  res.sendFile(path.join(__dirname, "all.html"));
-});
-
-// Search for Specific Character (or all characters) - provides JSON
-app.get("/api/:characters?", function(req, res) {
-  var chosen = req.params.characters;
-
-  if (chosen) {
-    console.log(chosen);
-
-    for (var i = 0; i < characters.length; i++) {
-      if (chosen === characters[i].routeName) {
-        res.json(characters[i]);
-        return;
-      }
-    }
-
-    res.json(false);
-  }
-  else {
-    res.json(characters);
-  }
-});
-
-// Create New Characters - takes in JSON input
-app.post("/api/new", function(req, res) {
-  var newcharacter = req.body;
-  newcharacter.routeName = newcharacter.name.replace(/\s+/g, "").toLowerCase();
-
-  console.log(newcharacter);
-
-  characters.push(newcharacter);
-
-  res.json(newcharacter);
-});
+require("./app/html-routes.js");
+require("./app/api-routes.js");
 
 // Starts the server to begin listening
 // =============================================================
