8c300c6d304b178b299e88a58cda9ba1198b401d
PocketThi3f <krslai@yahoo.com>
2017-01-11 16:28:33 -0800

    got the server to listen and work with node

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ var path = require("path");
 var app = express();
 var PORT = process.env.PORT || 1337;
 
+// Has user go to public folder to access the main html 
+app.use(express.static('app/public'));
+
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -18,8 +21,8 @@ app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 // Routes
 // =============================================================
 // Basic route that sends the user first to the AJAX Page
-require("./app/html-routes.js");
-require("./app/api-routes.js");
+require("./app/routing/html-routes.js")(app);
+require("./app/routing/api-routes.js")(app);
 
 // Starts the server to begin listening
 // =============================================================
